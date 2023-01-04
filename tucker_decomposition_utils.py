2723049f2dea8988660b0dd9298693b994a753b7
Matthew Fahrbach <matthew.fahrbach@gmail.com>
2023-01-04 11:21:53 -0500

    Delete tucker_decomposition_utils.py

--- tucker_decomposition_utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def get_num_tucker_params(tensor, core_shape):
-    num_tucker_params = np.prod(core_shape)
-    for n in range(len(core_shape)):
-        num_tucker_params += tensor.shape[n] * core_shape[n]
-    return num_tucker_params
